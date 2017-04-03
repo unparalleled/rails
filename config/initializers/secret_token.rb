3285eef24c85b170690c42e6e20781d37a749307
Kevin Thomas <unparalleled@users.noreply.github.com>
2017-04-03 15:43:40 -0700

    initial commit of rails sandbox repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1f95db97a0441ef114d1652bbf1920adff9b266b38b588cef7dc11d83a7fb305b28c49b80510c4e8006d7ee0580932a969c36e94bb91c4b53fc615ac2ef2eae6'
