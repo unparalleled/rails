31a3388d9de99957b615fdff32d0cc7f7698f8d1
Kevin Thomas <unparalleled@users.noreply.github.com>
2017-04-04 14:18:56 -0700

    added new session counter with reset logic and view

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+
+    session[:counter] = 
+    if session[:counter].nil? then 0
+      else session[:counter] + 1
+    end
+  
   end
 end
