0db74be6924e2693abaf44786e1188790fa8f8c5
Kevin Thomas <unparalleled@users.noreply.github.com>
2017-04-04 13:45:20 -0700

    added cart functionality with session tracking

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -10,4 +14,17 @@ class Product < ActiveRecord::Base
   	minimum: 10,
   	message: 'Title is too short! Must be at least 10 chars'
   }
+
+  private
+
+    # ensure that there are no lines items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
+
 end
